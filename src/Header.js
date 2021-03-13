494c430ddbad5a7e58646470d71eb19979faf56a
basilmaben <basilmaben.bm@gmail.com>
2021-03-13 20:54:22 +0530

    dummy menuItems added

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Logo from "./robinhood.svg";
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header__wrapper">
+      {/* logo */}
+      <div className="header__logo">
+        <img src={Logo} alt="" width={25} />
+      </div>
+      {/* search */}
+      <div className="header__search">
+        <div className="header__searchContainer">
+          <input placeholder="Search" type="text" />
+        </div>
+      </div>
+      {/* menu items */}
+      <div className="header__menuItems">
+          <a href = "#">Free Stocks</a>
+          <a href = "#">Portfolio</a>
+          <a href = "#">Cash</a>
+          <a href = "#">Messages</a>
+          <a href = "#">Account</a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
