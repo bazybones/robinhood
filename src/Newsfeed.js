cda6ede94a558187f84239de6f89580ba9d16285
basilmaben <basilmaben.bm@gmail.com>
2021-03-14 17:47:17 +0530

    Simple dummy prices added for display

---
+++ src/Newsfeed.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Newsfeed.css'
+import LineGraph from'./LineGraph'
+
+function Newsfeed() {
+    return (
+        <div className = "newsfeed">
+            <div className="newsfeed__container">
+                <div className="newsfeed__chartSection">
+                    <div className="newsfeed__portfolio">
+                        <h1>$114,656</h1>
+                        <p>$44.63 (+0.04%) Today</p>
+                    </div>
+                    <div className="newsfeed__chart"></div>
+                    <LineGraph/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Newsfeed
