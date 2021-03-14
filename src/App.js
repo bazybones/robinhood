aebff5387d8b6d4fda6a8ccae095172015737019
basilmaben <basilmaben.bm@gmail.com>
2021-03-14 17:45:31 +0530

    NewsFeed and Stats components added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
+import Newsfeed from "./Newsfeed";
+import Stats from "./Stats";
 
 function App() {
   return (
@@ -9,7 +10,13 @@ function App() {
       <div className="app__header">
         <Header />
       </div>
-      <div className="app__body"></div>
+      <div className="app__body">
+        <div className="app__container">
+          <Newsfeed />
+          {/* stats */}
+          <Stats />
+        </div>
+      </div>
       {/* Body */}
     </div>
   );
