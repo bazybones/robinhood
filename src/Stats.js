a1bee7b3c97fe063854826c17fdccccd920cd520
basilmaben <basilmaben.bm@gmail.com>
2021-03-14 17:48:12 +0530

    basic component structure

---
+++ src/Stats.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Stats() {
+    return (
+        <div className = "stats">
+            <div>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Stats
