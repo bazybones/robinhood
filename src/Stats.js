7e160705fdaab2f17093e2f26045942b0d2918a5
basilmaben <basilmaben.bm@gmail.com>
2021-03-15 21:14:21 +0530

    axios calls for API finnhub and displaying dummy

--- src/Stats.js
+++ src/Stats.js
@@ -1,14 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./Stats.css";
+import axios from "axios";
+import StatsRow from "./StatsRow";
+
+const TOKEN = "c17lkn748v6t1a48el40";
+const BASE_URL = "https://finnhub.io/api/v1/quote";
 
 function Stats() {
-    return (
-        <div className = "stats">
-            <div>
-                
-            </div>
-            
+  const [stockData, setstockData] = useState([]);
+
+  const getStocksData = (stock) => {
+    return axios
+      .get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`)
+      .catch((error) => {
+        console.error("Error", error.message);
+      });
+  };
+
+  useEffect(() => {
+    let tempStocksData = [];
+    const stocksList = [
+      "AAPL",
+      "MSFT",
+      "TSLA",
+      "FB",
+      "BABA",
+      "UBER",
+      "DIS",
+      "SBUX",
+    ];
+
+    /*    getMyStocks(); */
+    let promises = [];
+    stocksList.map((stock) => {
+      promises.push(
+        getStocksData(stock).then((res) => {
+          tempStocksData.push({
+            name: stock,
+            ...res.data,
+          });
+        })
+      );
+    });
+
+    Promise.all(promises).then(() => {
+      setstockData(tempStocksData);
+      console.log(tempStocksData);
+    });
+  }, []);
+
+  return (
+    <div className="stats">
+      <div className="stats__container">
+        <div className="stats__header">
+          <p>Stocks</p>
         </div>
-    )
+        <div className="stats__content">
+          <div className="stats__rows">
+              {stockData.map((stock)=>(<StatsRow/>))}
+            
+             {/* key={stock.data.ticker}
+                name={stock.data.ticker}
+                openPrice={stock.info.o}
+                volume={stock.data.shares}
+                price={stock.info.c}  */}
+            
+          </div>
+        </div>
+        <div className="stats__header">
+          <p>Lists</p>
+        </div>
+        <div className="stats__content">
+          <div className="stats__rows">{/* Stocks we can buy */}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Stats
+export default Stats;
