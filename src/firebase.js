945143de574948fa0bf0030894e4b82f6c8dba01
basilmaben <basilmaben.bm@gmail.com>
2021-03-16 11:21:27 +0530

    config firebase added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBiEu5aEJ_qHnDroVzxf3t2x-D95NppYNQ",
+    authDomain: "robinhood-21b22.firebaseapp.com",
+    projectId: "robinhood-21b22",
+    storageBucket: "robinhood-21b22.appspot.com",
+    messagingSenderId: "1087054547994",
+    appId: "1:1087054547994:web:a8201ff6043a0643e195a3"
+  };
+
+  
