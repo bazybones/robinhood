76e44c20cf240f7d6e03394eeef47cb5a587b234
basilmaben <basilmaben.bm@gmail.com>
2021-03-16 21:17:25 +0530

    add Stocks

--- src/StatsRow.js
+++ src/StatsRow.js
@@ -1,16 +1,44 @@
 import React from "react";
+import { db } from "./firebase";
 import StockChart from './stock.svg'
 
 function StatsRow(props) {
-  //   console.log(props, "what is in props here?");
-// (currentPrice - openPrice)/openPrice
+
   const percentage = ((props.price - props.openPrice)/props.openPrice) * 100;
 
-  const getModal = () => {
+  const buyStock = () => {
+    db.collection('myStocks')
+    .where("ticker","==", props.name)
+    .get()
+    .then((querySnapshot)=>{
+      if(!querySnapshot.empty){
+        
+        querySnapshot.forEach((doc) => {
 
-  }
+          // doc.data() is never undefined for query doc snapshots
+          db.collection('myStocks')
+        .doc(doc.id)
+        .update({
+          shares: doc.data().shares+=1
+        })
+
+      });
+      } else {
+        db.collection('myStocks')
+        .add({
+          ticker: props.name,
+          shares: 1
+        })
+      }
+      
+  })
+}
+  
+  /* const getModal = () => {
+
+  } */
   return (
-    <div className="row" onClick={getModal}>
+    <div className="row" onClick={buyStock}>
       <div className="row__intro">
         <h1>{props?.name}</h1>
         <p>{props.volume && 
