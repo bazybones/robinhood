b826d21b3b6e50d27949bb60f9f32d5451543f97
basilmaben <basilmaben.bm@gmail.com>
2021-03-16 11:35:33 +0530

    percent and props display

--- src/StatsRow.js
+++ src/StatsRow.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import './StatsRow.css'
+import StockSVG from './stock.svg'
 
-function StatsRow() {
+function StatsRow(props) {
+
+    const percentage = ((props.price - props.openPrice)/props.openPrice) * 100;
+    
     return (
         <div className="row" /* onClick={getModal} */>
         <div className="row__intro">
-          <h1>{/* {props?.name} */}AAPL</h1>
-          <p>{/* {props.volume && 
-            (props.volume + " shares")
-          } */}200 shares</p>
+          <h1>{props.name}</h1>
+          <p>{props.shares && 
+            (props.shares + " shares")
+          }</p>
         </div>
         <div className="row__chart">
-         {/*  <img src={StockChart} height={16}/> */}
+          <img src={StockSVG} alt="" height={16}/>
         </div>
         <div className="row__numbers">
-          <p className="row__price">{/* {props.price} */} $200</p>
-          <p className="row__percentage"> +{/* {Number(percentage).toFixed(2)} */}200%</p>
+          <p className="row__price">{props.price}</p>
+          <p className="row__percentage"> +{Number(percentage).toFixed(2)}</p>
         </div>
       </div>
     )
